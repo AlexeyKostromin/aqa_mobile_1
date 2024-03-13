41af510842ff6891a4b7fddee74c9f63eaeb6129
Alexey Kostromin <kostromin.aleksei@gmail.com>
2024-03-13 21:30:52 +0100

    Added tests testGetClassNumber() and estGetClassString()

--- src/test/java/tests/MainClassTest.java
+++ src/test/java/tests/MainClassTest.java
@@ -1,13 +1,37 @@
 package tests;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class MainClassTest extends MainClass{
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainClassTest extends MainClass {
 
     @Test
     void testGetLocalNumber() {
+        final int expectedInt = 14;
         int localNumber = this.getLocalNumber();
-        Assertions.assertEquals(14, localNumber, "Expected number is 14, but was" + localNumber);
+        assertEquals(expectedInt, localNumber, "Expected number is " + expectedInt + ", but was " + localNumber);
     }
+
+    @Test
+    void testGetClassNumber() {
+        final int expectedInt = 45;
+        int classNumber = this.getClassNumber();
+        assertTrue(classNumber > expectedInt, "classNumber " + classNumber + " is not greater than " + expectedInt);
+    }
+
+    @Test
+    void testGetClassString() {
+        String classString = this.getClassString();
+        assertTrue(classString.contains("hello") || classString.contains("Hello"),
+                "String should contain 'hello' or 'Hello' but was " + classString);
+    }
+
 }
+
+
+
+
+
+
