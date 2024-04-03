36edc987271c1e0b857d867e15cd4e5c0319e0d8
Alexey Kostromin <kostromin.aleksei@gmail.com>
2024-04-03 22:24:49 +0200

    added swipeArticleTest()

--- src/test/java/tests/Lesson3Tests.java
+++ src/test/java/tests/Lesson3Tests.java
@@ -26,7 +26,6 @@ public class Lesson3Tests extends TestBase {
 
         waitWelcomePageLoaded();
         skipWelcomePage();
-        clickSearchTextBox();
         performSearchWithText(searchText);
         verifySearchResultsContains(expectedResult);
         clickSearchCloseButton();
@@ -39,13 +38,89 @@ public class Lesson3Tests extends TestBase {
 
         waitWelcomePageLoaded();
         skipWelcomePage();
-        clickSearchTextBox();
         performSearchWithText(searchText);
-        var searchResults = getAllSearchResults();
-        verifyResultsContainsText(searchResults, searchText);
+        var searchResultsList = getAllSearchResultsByTitle();
+        verifyResultsContainsTextInTitle(searchResultsList, searchText);
+    }
+
+    @Test
+    void swipeArticleTest() {
+        final String searchText1 = "Intellij IDEA";
+        final String expectedResult1 = "Integrated development environment";
+
+        waitWelcomePageLoaded();
+        skipWelcomePage();
+
+        performSearchWithText(searchText1);
+        tapItemByText(expectedResult1);
+        swipeUpToTheEndOfArticle();
+    }
+
+    @Test
+    void saveTwoArticlesToListTest() {
+        final String searchText1 = "Java";
+        final String expectedResult1 = "Java (programming language)";
+        final String searchText2 = "Kotlin";
+        final String expectedResult2 = "Kotlin (programming language)";
+
+        waitWelcomePageLoaded();
+        skipWelcomePage();
+
+        performSearchWithText(searchText1);
+        tapItemByText(expectedResult1);
+
+        final String listName = "my list1";
+        saveArticleToNewList(listName);
+        clickNavigateUp();
+
+        sendKeysToSearchTextBox(searchText2);
+        tapItemByText(expectedResult2);
+        saveArticleToExistingList(listName);
+
+        clickNavigateUp();
+        clickNavigateUp();
+
+        goToSavedItems();
+        tapItemByText(listName);
+        submitTooltipGotIt();
+
+        verifyArticleDisplays(expectedResult1);
+        verifyArticleDisplays(expectedResult2);
+
+        swipeElementToLeft(getElementByText(expectedResult1));
+        waitArticleNotDisplays(expectedResult1);
+        verifyArticleDisplays(expectedResult2);
+
+        tapItemByText(expectedResult2);
+        waitArticlePageLoaded();
+        verifyArticleTitle(expectedResult2);
+    }
+
+    public void saveArticleToNewList(String listName) {
+        clickSaveArticle();
+        clickAddToList();
+        setNewListName(listName);
+    }
+
+    public void saveArticleToExistingList(String listName) {
+        clickSaveArticle();
+        clickAddToList();
+        tapItemByText(listName);
+    }
+
+    public void waitArticlePageLoaded() {
+        waitForElementPresent(
+                By.id("org.wikipedia.alpha:id/page_toolbar_button_search"),
+                "Toolbar search not found",
+                10);
     }
 
     public void performSearchWithText(String value) {
+        clickSearchTextBox();
+        sendKeysToSearchTextBox(value);
+    }
+
+    public void sendKeysToSearchTextBox(String value) {
         waitForElementAndSendKeys(
                 By.id("org.wikipedia.alpha:id/search_src_text"),
                 value,
@@ -108,23 +183,112 @@ public class Lesson3Tests extends TestBase {
         );
     }
 
-    public List<WebElement> getAllSearchResults() {
+    public List<WebElement> getAllSearchResultsByTitle() {
         return waitForElementsPresent(
                 By.id("org.wikipedia.alpha:id/page_list_item_title"),
                 "Could not get search results",
                 15
         );
-
     }
 
-    public void verifyResultsContainsText(List<WebElement> elements, String expectedText) {
-        for (WebElement element : elements) {
-            Assertions.assertTrue(element.getText().contains(expectedText),
-                    "Expected text for element should contain: " + expectedText
-                            + "\n but text was: " + element.getText());
-        }
-
+    public List<WebElement> getAllSearchResults() {
+        return waitForElementsPresent(
+                By.xpath("//*[@resource-id='org.wikipedia.alpha:id/search_results_list']/android.view.ViewGroup"),
+                "Could not get search results",
+                15
+        );
     }
+
+    public void clickArticleTitleWithText(List<WebElement> elements, String text) {
+        findElementByTextInList(elements, text).click();
+    }
+
+    public void clickSaveArticle() {
+        waitForElementAndClick(
+                By.id("org.wikipedia.alpha:id/page_save"),
+                "Could not click add save article",
+                5);
+    }
+
+    public void clickSnackbarAction() {
+        waitForElementAndClick(
+                By.id("org.wikipedia.alpha:id/snackbar_action"),
+                "Could not click snackbar action",
+                5);
+    }
+
+    public void clickAddToList() {
+        waitForElementAndClick(
+                By.xpath("//*[@text='Add to list']"),
+                "Could not click add to list",
+                5);
+    }
+
+    public void setNewListName(String text) {
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia.alpha:id/text_input"),
+                text, "Could not set name for a new list",
+                10);
+        waitForElementAndClick(
+                By.xpath("//android.widget.Button[@text='OK']"),
+                "Could not press OK in new list dialog",
+                10);
+    }
+
+    public void clickNavigateUp() {
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Could not press Navigate Up",
+                5);
+    }
+
+    public void goToSavedItems() {
+        waitForElementAndClick(
+                By.id("org.wikipedia.alpha:id/nav_tab_reading_lists"),
+                "Could not press Navigate Up",
+                5);
+    }
+
+    public void submitTooltipGotIt() {
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia.alpha:id/balloon_card']//*[@text='Got it']"),
+                "Could not submit Got it",
+                5);
+    }
+
+    public void swipeUpToTheEndOfArticle() {
+        swipeUpToElement(
+                By.xpath("//*[@text = 'ABOUT THIS ARTICLE']"),
+                20,
+                "Could no swipe to element with text: 'ABOUT THIS ARTICLE'");
+    }
+
+    public void waitArticleNotDisplays(String article) {
+        String xpathFull = String.format("//*[@text='%s']", article);
+        waitForElementNotPresent(
+                By.xpath(xpathFull),
+                "Article with text: " + article + " was displayed, but should not",
+                10);
+    }
+
+    public void verifyArticleDisplays(String article) {
+        getElementByText(article);
+    }
+
+    public WebElement verifyResultsContainsTextInTitle(List<WebElement> elements, String expectedText) {
+        var element = findElementByTextInList(elements, expectedText);
+
+        Assertions.assertTrue(element.getText().contains(expectedText),
+                "Expected text for element should contain: " + expectedText
+                        + "\n but text was: " + element.getText());
+
+        return element;
+    }
+
+    public void verifyArticleTitle(String title) {
+        getElementByText(title);
+    }
+
 }
 
 
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -5,7 +5,10 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -14,7 +17,7 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,6 +25,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestBase {
 
     public AppiumDriver driver;
+
 
     @BeforeEach
     public void setUp() throws MalformedURLException {
@@ -93,6 +97,145 @@ public class TestBase {
         return element;
     }
 
+    public WebElement getElementByText(String text) {
+        String xpathWithName = String.format("//*[@text='%s']", text);
+        return waitForElementPresent(By.xpath(xpathWithName),
+                "Could not find element by text: " + text,
+                10);
+    }
+
+    public void tapItemByText(String text) {
+        String xpathWithName = String.format("//*[@text='%s']", text);
+        waitForElementAndClick(
+                By.xpath(xpathWithName),
+                "Could not select item with text: " + text,
+                5);
+    }
+
+
+    public WebElement findElementByTextInList(List<WebElement> elements, String text) {
+        for (WebElement element : elements) {
+            var elementText = element.getText();
+            if (elementText.contains(text)) {
+                return element;
+            }
+        }
+        throw new AssertionError("Could not find element with text: " + text);
+    }
+
+    public void swipeUp(int timeOfScroll) {
+        Dimension size = driver.manage().window().getSize();
+        int centerX = size.width / 2;
+        int startY = (int) (size.height * 0.70);
+        int endY = (int) (size.height * 0.30);
+
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+        Sequence swipe = new Sequence(finger, 1)
+                .addAction(finger.createPointerMove(Duration.ofSeconds(0), PointerInput.Origin.viewport(), centerX, startY))
+                .addAction(finger.createPointerDown(0))
+                .addAction(finger.createPointerMove(Duration.ofMillis(timeOfScroll), PointerInput.Origin.viewport(), centerX, endY))
+                .addAction(finger.createPointerUp(0));
+
+        driver.perform(Arrays.asList(swipe));
+    }
+
+    public void scrollUp(int timeOfSwipe) {
+        Dimension size = driver.manage().window().getSize();
+        int startX = size.width / 2;
+        int startY = (int) (size.height * 0.8);
+        int endY = (int) (size.height * 0.2);
+
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+        Sequence swipe = new Sequence(finger, 0);
+
+        swipe.addAction(finger.createPointerMove(Duration.ofMillis(0), PointerInput.Origin.viewport(), startX, startY));
+        swipe.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+        swipe.addAction(finger.createPointerMove(Duration.ofMillis(timeOfSwipe), PointerInput.Origin.viewport(), startX, endY));
+        swipe.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        driver.perform(Arrays.asList(swipe));
+    }
+
+    public void swipeUpToElement(By by, int maxSwipes, String errorMessage) {
+        int swipesCount = 0;
+
+        while (driver.findElements(by).size() == 0) {
+            swipeUp(200);
+            ++swipesCount;
+
+            if (swipesCount > maxSwipes) {
+                throw new AssertionError("Could not find element by swiping up.\n" + errorMessage);
+            }
+        }
+    }
+
+    public void swipeElementToLeft(WebElement element) {
+        int leftX = element.getLocation().getX();
+        int rightX = leftX + element.getSize().getWidth();
+        int upperY = element.getLocation().getY();
+        int lowerY = upperY + element.getSize().getHeight();
+        int middleY = (upperY + lowerY) / 2;
+
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+        Sequence swipe = new Sequence(finger, 1)
+                .addAction(finger.createPointerMove(Duration.ofSeconds(0), PointerInput.Origin.viewport(), rightX, middleY))
+                .addAction(finger.createPointerDown(0))
+                .addAction(finger.createPointerMove(Duration.ofMillis(150), PointerInput.Origin.viewport(), leftX, middleY))
+                .addAction(finger.createPointerUp(0));
+
+        driver.perform(Arrays.asList(swipe));
+    }
+
+    public void swipeElementToLeft(By by, String errorMessage) {
+        WebElement element = waitForElementPresent(by,
+                errorMessage,
+                10);
+
+        int leftX = element.getLocation().getX();
+        int rightX = leftX + element.getSize().getWidth();
+        int upperY = element.getLocation().getY();
+        int lowerY = upperY + element.getSize().getHeight();
+        int middleY = (upperY + lowerY) / 2;
+
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+        Sequence swipe = new Sequence(finger, 1)
+                .addAction(finger.createPointerMove(Duration.ofSeconds(0), PointerInput.Origin.viewport(), rightX, middleY))
+                .addAction(finger.createPointerDown(0))
+                .addAction(finger.createPointerMove(Duration.ofMillis(150), PointerInput.Origin.viewport(), leftX, middleY))
+                .addAction(finger.createPointerUp(0));
+
+        driver.perform(Arrays.asList(swipe));
+    }
+
+    public void zoom(int x, int y) {
+        Dimension size = driver.manage().window().getSize();
+        int startX1 = x - (size.width / 4);
+        int startY1 = y;
+        int startX2 = x + (size.width / 4);
+        int startY2 = y;
+
+        PointerInput finger1 = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
+        PointerInput finger2 = new PointerInput(PointerInput.Kind.TOUCH, "finger2");
+
+        Sequence finger1Sequence = new Sequence(finger1, 0);
+        Sequence finger2Sequence = new Sequence(finger2, 1);
+
+        // Finger 1 actions (move finger away from the center)
+        finger1Sequence.addAction(finger1.createPointerMove(Duration.ofMillis(0), PointerInput.Origin.viewport(), startX1, startY1));
+        finger1Sequence.addAction(finger1.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+        finger1Sequence.addAction(finger1.createPointerMove(Duration.ofMillis(600), PointerInput.Origin.viewport(), startX1 - 100, startY1));
+        finger1Sequence.addAction(finger1.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        // Finger 2 actions (move finger away from the center)
+        finger2Sequence.addAction(finger2.createPointerMove(Duration.ofMillis(0), PointerInput.Origin.viewport(), startX2, startY2));
+        finger2Sequence.addAction(finger2.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+        finger2Sequence.addAction(finger2.createPointerMove(Duration.ofMillis(600), PointerInput.Origin.viewport(), startX2 + 100, startY2));
+        finger2Sequence.addAction(finger2.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        // Perform the zoom gesture
+        driver.perform(Arrays.asList(finger1Sequence, finger2Sequence));
+    }
+
     public void assertElementHasText(By by, String expectedText) {
         WebElement element = waitForElementPresent(by, "Failed to locate element");
         String elementText = element.getAttribute("text");
@@ -100,6 +243,7 @@ public class TestBase {
                 expectedText, elementText,
                 "Expected text for element is: " + expectedText + " but was: " + elementText);
     }
+
 
 
 }
